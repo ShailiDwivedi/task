f7236a376f29d38610a33982d3e2545a25d20a97
Shaili <shaili.d@zecdata.com>
2024-06-05 18:48:18 +0530

    request module added into task.py

--- task.py
+++ task.py
@@ -12,3 +12,4 @@ def get_api_data(api):
         print("Response content:", response.content)
 
 get_api_data("https://api.worldbank.org/v2/country/ARG;BOL;BRA;CHL;COL;ECU;GUY;PRY;PER;SUR;URY;VEN/indicator/NY.GDP.MKTP.CD?format=json&page=1&per_page=50")
+
